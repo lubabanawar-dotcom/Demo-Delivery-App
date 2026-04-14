1758dea081f97f8824f9c23356b6fd7f4575e1ec
lubabanawar-dotcom <lubaba.chowdhury.252@northsouth.edu>
2026-04-14 10:24:58 +0600

    Initial Project Setup

---
+++ src/delivery/DeliveryRepository.java
@@ -0,0 +1,27 @@
+/*package delivery;
+
+import java.util.List;
+
+public interface DeliveryRepository {
+	void save(Delivery delivery);
+	
+	Delivery findById(int id);
+	
+	List<Delivery> findAll();
+	
+	void update(Delivery delivery);
+	
+	void delete(int id);
+	
+	boolean exists(int id);
+	
+	int count();
+	
+	void clear();
+	
+	List<Delivery> findByOrderId(int orderId);
+	
+	List<Delivery> findByStatus(String status);
+
+}
+*/
--- src/user/UserTest.java
+++
@@ -1,7 +0,0 @@
-package user;
-
-public class UserTest{
-	public static void main(String [] args) {
-		
-	}
-}
