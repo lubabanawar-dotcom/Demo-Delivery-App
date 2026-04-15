8d8654eb4d08e810b6ec7c420e4ecd81053fe145
lubabanawar-dotcom <lubaba.chowdhury.252@northsouth.edu>
2026-04-15 17:01:46 +0600

    Fixed project structure and implemented Order module

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,22 @@
 package app;
 
+import java.util.Arrays;
+import order.Order;
+import order.OrderManager;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Food Delivery System Running...");
-	}
+public static void main(String[] args) {
+
+
+    OrderManager manager = new OrderManager();
+
+    Order order1 = new Order(1, 101, Arrays.asList(1,2), 500.0, "PLACED");
+
+    manager.addOrder(order1);
+
+    for (Order o : manager.getAllOrders()) {
+        System.out.println("Order ID: " + o.getId());
+    }
+}
+
 }
--- src/order/Order.java
+++ src/order/Order.java
@@ -1,1 +1,39 @@
 package order;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Order implements Serializable {
+
+private int id;
+private int userId;
+private List<Integer> itemIds;
+private double totalAmount;
+private String status;
+
+public Order() {}
+
+public Order(int id, int userId, List<Integer> itemIds, double totalAmount, String status) {
+    this.id = id;
+    this.userId = userId;
+    this.itemIds = itemIds;
+    this.totalAmount = totalAmount;
+    this.status = status;
+}
+
+public int getId() { return id; }
+public void setId(int id) { this.id = id; }
+
+public int getUserId() { return userId; }
+public void setUserId(int userId) { this.userId = userId; }
+
+public List<Integer> getItemIds() { return itemIds; }
+public void setItemIds(List<Integer> itemIds) { this.itemIds = itemIds; }
+
+public double getTotalAmount() { return totalAmount; }
+public void setTotalAmount(double totalAmount) { this.totalAmount = totalAmount; }
+
+public String getStatus() { return status; }
+public void setStatus(String status) { this.status = status; }
+
+}
