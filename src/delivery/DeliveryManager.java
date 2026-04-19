d9b587707c1c5a0d9d1a084b3ab5d7710bdce294
ak16ibrashid23-pixel <ak16ibrashid23@gmail.com>
2026-04-20 00:54:35 +0600

    janina ki likhbo

--- src/delivery/DeliveryManager.java
+++ src/delivery/DeliveryManager.java
@@ -1,5 +1,37 @@
 package delivery;
+import java.util.List;
 
 public class DeliveryManager {
+	
+	public DeliveryRepository repository;
+	
+	public DeliveryManager(DeliveryRepository repository)
+	{
+		this.repository=repository;
+	}
+	
+	public void createNewDelivery(String orderId,String id,String address) 
+	{
+		Delivery d=new Delivery(orderId,id,address);
+		repository.save(d);
+		System.out.println("Delivery created for address: " + address);
+		
+	}
+	
+	public void completeDelivery(String id) 
+	{
+		Delivery d=repository.findById(id);
+		
+		if(d!=null)
+		{
+			d.setstatus("Delivered");
+			System.out.println("Order " + id + " has been dropped off");
+		}
+	}
+		public List<Delivery> getAllDeliveries()
+		{
+	        return repository.findAll();
+		
+	    }
 
 }
